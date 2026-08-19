5cf1b6f41897f1dab53365b39c358e898084c811
SiraGuide <siraphob.t39@gmail.com>
2026-08-19 09:38:32 +0700

    chore: ignore non-project and metadata files

--- test-sync.js
+++
@@ -1,4 +0,0 @@
-const { SheetsDB } = require('./supabase-sync.js');
-
-// A quick hack since supabase-sync.js is an ES module and we want to test it locally.
-// Actually, let's just write a test script that uses dynamic import.
